c1255fe9257a2680685c87984cc55d03f1634fae
Royce <royce20002006@yahoo.com>
2024-09-05 23:29:48 -0500

    working on likes finished with get

---
+++ app/api/likes_routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, session
+from flask_login import login_required
+from app.models import Like, db
+
+likes_routes = Blueprint('likes', __name__)
+
+
+@likes_routes.route('<int:id>/likes')
+def likes(id):
+    """
+    Query for all likes and returns them in a list of user dictionaries
+    by post id
+    """
+    likes = Like.query.filter_by(post_id=id).all()
+    
+
+    return {'likes': [like.to_dict() for like in likes]}
+
+
+@likes_routes.route('<int:id>/likes', methods=['get', 'post'])
+@login_required
+def user(id):
+    """
+    Query for a user by id and returns that user in a dictionary
+
+    """
+    # user= session['user_id']
+
+   
+    new_like = Like(post_id=id, user_id=user)
+    db.session.add(new_like)
+    db.session.commit()
+    return user.to_dict()
