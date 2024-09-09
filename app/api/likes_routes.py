84a0d06f4b544a564f7b4757f80f7ab79c6ead97
Royce <royce20002006@yahoo.com>
2024-09-08 20:25:48 -0500

    work

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session
-from flask_login import login_required
+from flask_login import current_user, login_required
 from app.models import Like, db
 
 likes_routes = Blueprint('likes', __name__)
@@ -24,7 +24,7 @@ def user(id):
     Query for a user by id and returns that user in a dictionary
 
     """
-    # user= session['user_id']
+    user = current_user.to_dict()
 
    
     new_like = Like(post_id=id, user_id= str(user["id"]))
